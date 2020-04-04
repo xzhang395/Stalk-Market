7054d9507890d69cd085ce78ad756823cf04574f
xzhang395 <xuetingzhang313@gmail.com>
2020-04-04 15:22:37 -0700

    refaactor completed. need to add new timer/session rules

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,43 +4,39 @@ import pig from '../img/daisy.png'
 import Form from './Form.js'
 import Rank from './Rank.js'
 import Header from './Header.js'
-// import Firebase from '../components/firebase.js'
+import Firebase from '../components/firebase.js'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = { userData: [], text: '' };
     this.state = {
-        test:1
- };
-  
+      userData: [{
+        price: 0,
+        island: "",
+        name: ""
+      }
+      ]
+    };
+
+  }
+  getUserData = () => {
+    var today = new Date();
+    var ref = Firebase.database().ref("market/" + composeHashKey(today) + "/");
+    let array = [];
+    ref.on('value', snapshot => {
+      const dataset = snapshot.val();
+      var keys = Object.keys(dataset);
+      for (var i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        array.push(dataset[k]);
+      }
+      array.sort(compareEntry); // this array is sorted from highest -> lowest, starting with 0
+      this.setState({ userData: array });
+    });
   }
 
-  // componentDidMount() {
-  //   this.GetData().then(res =>
-  //     this.setState({data:3)
-  //   )
-  //   // this.getUserData();
-  //   this.setState({ test:3 }, () =>
-  //   this.render()
-  // );
-  // console.log("outside callback" +this.state.test);
-      
-  // }
-  // GetData(){
-  //   this.setState({test: 3})
-  // }
-  // componentDidMount() {
-  //   this.GetData().then(res =>
-  //     this.setState({test: 3})
-  //   )
-  // }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(
-      `this.state.clickCounts(♻️ componentDidUpdate)`,
-      this.state.test
-    );
+  componentDidMount() {
+    this.getUserData();
   }
 
   render() {
@@ -48,34 +44,36 @@ class App extends React.Component {
       <div>
         <div className="container">
           <img className="pig" src={pig} />
-          <Header data={this.state.test} />
+          <Header data={this.state.userData} />
           <Form />
         </div>
         <div className="ranking">
           <span className="rank-title">Current price around the world</span>
-          <Rank />
+          <Rank data={this.state.userData} />
         </div>
       </div>
     );
   }
 }
-// function compareEntry(a, b) {
-//   if (Number(a.price) > Number(b.price)) return -1;
-//   if (Number(b.price) > Number(a.price)) return 1;
-//   return 0;
-// }
 
-// function composeHashKey(date) {
-//   var fullDate = composeDateString(date);
-//   if (date.getHours() < 12) {
-//     return fullDate + "-0"; // morning price
-//   }
-//   return fullDate + "-1"; // afternoon price
-// }
-// function composeDateString(date) {
-//   var dd = String(date.getDate()).padStart(2, "0");
-//   var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
-//   var yyyy = date.getFullYear();
-//   return yyyy + "-" + mm + "-" + dd;
-// }
+//compose data key
+function compareEntry(a, b) {
+  if (Number(a.price) > Number(b.price)) return -1;
+  if (Number(b.price) > Number(a.price)) return 1;
+  return 0;
+}
+
+function composeHashKey(date) {
+  var fullDate = composeDateString(date);
+  if (date.getHours() < 12) {
+    return fullDate + "-0"; // morning price
+  }
+  return fullDate + "-1"; // afternoon price
+}
+function composeDateString(date) {
+  var dd = String(date.getDate()).padStart(2, "0");
+  var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = date.getFullYear();
+  return yyyy + "-" + mm + "-" + dd;
+}
 export default App;
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,68 +1,25 @@
 import React from 'react';
 import Timer from '../components/Timer.js'
-import Firebase from '../components/firebase.js'
 
 class Header extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { userData: [{price:12}] };
-    }
-
-      getUserData = () => {
-    var today = new Date();
-    var ref = Firebase.database().ref("market/" + composeHashKey(today) + "/");
-    let array = [];
-    ref.on('value', snapshot => {
-      const dataset = snapshot.val();
-      var keys = Object.keys(dataset);
-      for (var i = 0; i < keys.length; i++) {
-        var k = keys[i];
-        array.push(dataset[k]);
-      }
-      array.sort(compareEntry); // this array is sorted from highest -> lowest, starting with 0
-      // console.log(array);
-      this.setState({ userData: array });
-    });
+  constructor(props) {
+    super(props);
   }
-    
-    componentDidMount() {
-      this.getUserData();
-      console.log("header value "+this.props.data);
-    }
 
-    componentWillReceiveProps({someProp}) {
-      this.setState({...this.state,someProp})
-    }
-    render() {
-      return (
-        <div className="Header">
-        <span id="max-price">{this.state.userData[0].price}</span>
+  componentWillReceiveProps({ someProp }) {
+    this.setState({ ...this.state, someProp })
+  }
+
+  render() {
+    return (
+      <div className="Header">
+        <span id="max-price">{this.props.data[0].price}</span>
         <br />
-        <span id="best-island">at {this.state.userData[0].island}</span>
+        <span id="best-island">at {this.props.data[0].island}</span>
         <Timer />
       </div>
-      );
-    }
+    );
   }
+}
 
-  //TODO: move to app.js
-  function compareEntry(a, b) {
-    if (Number(a.price) > Number(b.price)) return -1;
-    if (Number(b.price) > Number(a.price)) return 1;
-    return 0;
-  }
-  
-  function composeHashKey(date) {
-    var fullDate = composeDateString(date);
-    if (date.getHours() < 12) {
-      return fullDate + "-0"; // morning price
-    }
-    return fullDate + "-1"; // afternoon price
-  }
-  function composeDateString(date) {
-    var dd = String(date.getDate()).padStart(2, "0");
-    var mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
-    var yyyy = date.getFullYear();
-    return yyyy + "-" + mm + "-" + dd;
-  }
-  export default Header;
+export default Header;
