384af0b054098407f978d5eb52bb484e5f025b92
xzhang395 <xuetingzhang313@gmail.com>
2020-04-11 16:56:49 -0700

    didn't commit everything needed for auth branch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+// import Firebase, { FirebaseContext } from './components/Firebase';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import Navigation from './components/Navigation/index.js';
+import LandingPage from './components/Landing';
+import SignUpPage from './components/SignUp';
+import SignInPage from './components/SignIn';
+import PasswordForgetPage from './components/PasswordForget';
+import HomePage from './components/Home';
+import AccountPage from './components/Account';
+import AdminPage from './components/Admin';
+import * as ROUTES from './constants/routes.js';
+
 // require('dotenv').config();
-
-
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <FirebaseContext.Provider value={new Firebase()}> */}
+    <Router>
+            <div>
+              <Navigation />
+              <Route exact path={ROUTES.LANDING} component={App} />
+              <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+              <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+              <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+              <Route path={ROUTES.HOME} component={HomePage} />
+              <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+              <Route path={ROUTES.ADMIN} component={AdminPage} />
+            </div>
+          </Router>
+      {/* <App /> */}
+    {/* </FirebaseContext.Provider>, */}
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -18,3 +43,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+         
