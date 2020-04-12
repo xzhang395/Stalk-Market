5ac1599cd32bed1f193a3b303a339e4a6f14ce56
Mark Qi <markqi@amazon.com>
2020-04-11 23:01:03 -0700

    removing database creds

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,20 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-    // apiKey: process.env.REACT_APP_API_KEY,
-    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    // databaseURL: process.env.REACT_APP_DATABASE_URL,
-    // projectId: process.env.REACT_APP_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    apiKey: "AIzaSyAI2G1hIMEqkk9B7dAIVQfbJiDjdv-MHxI",
-    authDomain: "stalk-market-4dc91.fsirebaseapp.com",
-    databaseURL: "https://stalk-market-4dc91.firebaseio.com",
-    projectId: "stalk-market-4dc91",
-    storageBucket: "stalk-market-4dc91.appspot.com",
-    messagingSenderId: "458780571277",
-    appId: "1:458780571277:web:4cac1c4df036eb5a6054c4",
-    measurementId: "G-2RG24TVT9X"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 export const firebaseApp = firebase.initializeApp(config);
 export const auth = firebase.auth();
